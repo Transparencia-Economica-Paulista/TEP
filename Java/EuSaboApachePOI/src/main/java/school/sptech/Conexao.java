aceac6f4388dd51c5796e0590e1d84813a837bc2
euclidesRocha <euclides.rocha@sptech.school>
2025-10-24 17:36:15 -0300

    Mandando jar e codigo atualizado

--- Java/EuSaboApachePOI/src/main/java/school/sptech/Conexao.java
+++ Java/EuSaboApachePOI/src/main/java/school/sptech/Conexao.java
@@ -17,13 +17,13 @@ public class Conexao{
         DriverManagerDataSource driver = new DriverManagerDataSource();
 
         // Informamos o "jdbc:mysql", que é padrão. Depois o Hostname, porta e nome da Database
-        driver.setUrl("jdbc:mysql://localhost:3306/Laboratorio");
+        driver.setUrl("jdbc:mysql:54.175.109.229:3306/TEP");
         // Nome do usuário, EX: root
-        driver.setUsername("EuTeste");
+        driver.setUsername("root");
         // Por fim, informamos a senha.
         driver.setPassword("urubu100");
 
-        //Dizemos que o valor da conexao vai ser igual driver
+        //Declaramos que o valor da conexao vai ser igual driver
         this.conexao = driver;
     }
 
